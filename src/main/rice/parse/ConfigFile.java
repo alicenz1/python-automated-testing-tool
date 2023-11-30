662a0a2ab016f146233c94fbc6b36861bc1992c3
alice zhong <anz2@rice.edu>
2023-11-30 00:40:31 -0600

    start

--- src/main/rice/parse/ConfigFile.java
+++ src/main/rice/parse/ConfigFile.java
@@ -1,3 +1,33 @@
 package main.rice.parse;
+import main.rice.node.APyNode;
+import java.util.*;
+
+public class ConfigFile {
+
+    private String funcName;
+    private List<APyNode<?>> nodes;
+    private int numRand;
+
+    public ConfigFile(String funcName, List<APyNode<?>> nodes, int numRand) {
+        this.funcName = funcName;
+        this.nodes = nodes;
+        this.numRand = numRand;
+
+    }
+
+    public String getFuncName() {
+        return this.funcName;
+    }
+
+    public List<APyNode<?>> getNodes() {
+        return this.nodes;
+    }
+
+    public int getNumRand() {
+        return this.numRand;
+    }
+
+
+}
 
 // TODO: implement the ConfigFile class here
--- src/main/rice/parse/ConfigFileParser.java
+++ src/main/rice/parse/ConfigFileParser.java
@@ -1,3 +1,16 @@
 package main.rice.parse;
 
+import java.io.IOException;
+
+public class ConfigFileParser {
+
+    public static String readFile(String filepath) throws IOException {
+        return null;
+    }
+
+    public static ConfigFile parse(String contents) throws InvalidConfigException {
+        return null;
+    }
+}
+
 // TODO: implement the ConfigFileParser class here
